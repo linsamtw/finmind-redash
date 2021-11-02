77994e666d865a94cef8e4c248e3b52f97b1a546
sam <samlin266118@gmail.com>
2021-11-03 00:05:00 +0800

    feat: update stock price

--- stock_price.sql
+++ stock_price.sql
@@ -1,4 +1,5 @@
 SELECT stock_price.Close AS '股價',
+       stock_price.TradeVolume AS "交易量",
        stock_price.date AS date
 FROM taiwan_stock_price AS stock_price
 INNER JOIN taiwan_stock_info AS si ON si.stock_id = stock_price.StockID
