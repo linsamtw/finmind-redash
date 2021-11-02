0eefd5d0859a76eea19734ce80a9e95cd9d2492c
sam <samlin266118@gmail.com>
2021-11-03 00:03:28 +0800

    feat: add stock price

--- stock_price.sql
+++ stock_price.sql
@@ -1,4 +1,6 @@
-SELECT Close, Date
-FROM taiwan_stock_price
-WHERE StockID = '2330'
-order by Date
+SELECT stock_price.Close AS '股價',
+       stock_price.date AS date
+FROM taiwan_stock_price AS stock_price
+INNER JOIN taiwan_stock_info AS si ON si.stock_id = stock_price.StockID
+WHERE si.stock_name = '台積電'
+  AND stock_price.date >= '2015-01-01'
